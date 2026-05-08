53353012448c798cce3e8a027590f787e22d5493
Drlani02 <czolny506@gmail.com>
2026-05-08 14:30:16 +0200

    Multiplayer

--- Assets/LobbyManager.cs
+++ Assets/LobbyManager.cs
@@ -1,16 +1,86 @@
-using UnityEngine;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using TMPro;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using UnityEngine;
 
 public class LobbyManager : MonoBehaviour
 {
-    public void StartHost()
+    public TextMeshProUGUI JoinCodeText;
+    public TMP_InputField JoinCodeInputField;
+    public GameObject StartButton;
+    public GameObject PlayerPrefab;
+    public async Task Authenticate()
     {
-        NetworkManager.Singleton.StartHost();
-        NetworkManager.Singleton.SceneManager.LoadScene("Map1", UnityEngine.SceneManagement.LoadSceneMode.Single);
+        await UnityServices.InitializeAsync();
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
     }
 
-    public void StartClient()
+    public async Task CreateGame()
     {
+        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(1);
+        string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+        JoinCodeText.text = joinCode;
+        StartButton.SetActive(true);
+        RelayServerData relayServerData = AllocationUtils.ToRelayServerData(allocation, "dtls");
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+        NetworkManager.Singleton.StartHost();
+        NetworkManager.Singleton.SceneManager.OnSceneEvent += OnSceneEvent;
+    }
+
+    public async Task JoinGame(string joinCode)
+    {
+        JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+        RelayServerData relayServerData = AllocationUtils.ToRelayServerData(joinAllocation, "dtls");
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
         NetworkManager.Singleton.StartClient();
+    }
+
+    public async void OnJoinButtonClick() 
+    {
+        string joinCode = JoinCodeInputField.text;
+        await JoinGame(joinCode);
+    }
+
+    public async void OnCreateButtonClick() 
+    {
+        await CreateGame();
+    }
+
+    public void StartGame()
+    {
+        if (NetworkManager.Singleton.IsHost)
+        {
+            NetworkManager.Singleton.SceneManager.LoadScene("Map1", UnityEngine.SceneManagement.LoadSceneMode.Single);
+        }
+    }
+
+    private void OnSceneEvent(SceneEvent sceneEvent)
+    {
+        if (sceneEvent.SceneEventType == SceneEventType.LoadComplete && sceneEvent.SceneName == "Map1")
+        {
+            if (NetworkManager.Singleton.IsHost)
+            {
+                GameObject[] spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+                int index = (int)sceneEvent.ClientId % spawnPoints.Length;
+                Transform selectedSpawn = spawnPoints[index].transform;
+                GameObject playerInstance = Instantiate(PlayerPrefab, selectedSpawn.position, selectedSpawn.rotation);
+                NetworkObject playerNetworkObject = playerInstance.GetComponent<NetworkObject>();
+                playerNetworkObject.SpawnWithOwnership(sceneEvent.ClientId);
+            }
+        }
+    }
+
+    public async void Start()
+    {
+        
+        await Authenticate();
     }
 }
